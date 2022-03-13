cf75beba7995ded44f55d21a739e0da093ad2ada
hansol <wkdqhrh00@naver.com>
2022-03-13 16:33:54 +0900

    이력서 업데이트

---
+++ content/capabiliity.js
@@ -0,0 +1,22 @@
+export const capabilities = [
+  {
+    id: 0,
+    content: "Java & Spring 조합으로 업무를 많이 하여 해당 스택으로 개발에 익숙합니다.",
+  },
+  {
+    id: 1,
+    content: "Cubrid, Oracle 의 RDBMS 경험, Redis 운영 경험, RabbitMQ 운영 경험을 가지고 있습니다.",
+  },
+  {
+    id: 2,
+    content: "Kotlin 언어를 좋아하여 Kotlin 으로도 개발 가능합니다.",
+  },
+  {
+    id: 3,
+    content: "인프라 설치를 위하여 Shell Script 를 다룬 경험이 있어 Linux Command 활용 능력을 가지고 있습니다.",
+  },
+  {
+    id: 4,
+    content: "Admin Page 나 API docs 를 직접 만들 정도의 JavaScript, React 기술을 알고 있습니다.",
+  }
+]
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,78 @@
-import Link from 'next/link'
+import { capabilities } from "../content/capabiliity";
+import { experiences } from "../content/experience";
+
 export default function Home() {
+
   return (
     <div>
-      Hello World.{' '}
-      <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
-        <a>About</a>
-      </Link>
+      <section>
+        <h1>장한솔</h1>
+        <div>
+          소프트웨어 엔지니어로 일하고 있습니다.
+          문제 해결 자체를 좋아하고 남아 있는 문제가 있으면 참지 못합니다.
+          비효율적인 업무를 싫어하고 생산성 향상에 많은 고민을 하고 있습니다.
+        </div>
+      </section>
+
+      <hr/>
+
+      <section>
+        <div>
+          <div>
+            <h2>경험</h2>
+          </div>
+          <div>
+            <h3>NAVER Cloud</h3>
+            <span>Backend Developer</span>
+            <br/>
+            <span>2019.11 ~ 현재</span>
+          </div>
+          <div>
+            {
+              <ul>
+                {
+                  experiences.map(experience =>
+                    <li key={experience.id}>
+                      <h4>{experience.title}</h4>
+                      <ul>
+                        {
+                          experience.contents.map(content =>
+                            <li key={content.id}>
+                              {content.content}
+                            </li>
+                          )
+                        }
+                      </ul>
+                    </li>)
+                }
+              </ul>
+            }
+          </div>
+        </div>
+      </section>
+
+      <hr/>
+
+      <section>
+        <div>
+          <h2>역량</h2>
+        </div>
+        <div>
+          <ul>
+            {
+              capabilities.map(capability => <li key={capability.id}>{capability.content}</li>)
+            }
+          </ul>
+        </div>
+      </section>
+
+      <hr/>
+
+      <footer>
+        <div>
+          <a href="https://github.com/pinus-densiflora">GitHub</a>
+        </div>
+      </footer>
     </div>
   )
 }
