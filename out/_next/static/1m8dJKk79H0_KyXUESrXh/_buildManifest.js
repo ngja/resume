5692b6a5efa76e3b3c31de915c01dc2828a60196
hansol <wkdqhrh00@naver.com>
2022-05-06 16:21:54 +0900

    Deploy Next.js to gh-pages

--- out/_next/static/1m8dJKk79H0_KyXUESrXh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2fa3537e44c4e5d8.js"],"/_error":["static/chunks/pages/_error-2f883067a14f4c4a.js"],"/about":["static/chunks/pages/about-469a57a8ac44de5d.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
